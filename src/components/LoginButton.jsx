1c866af113b7a34955fc95470198ac26de8a178a
jh8839 <jh8839@nyu.edu>
2023-12-15 08:29:51 -0500

    simpler auth system - no ext libraries

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,16 +1,55 @@
 //LoginButton.js
+import React, { Component } from 'react';
+import md5 from 'md5';
 
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+// const LoginButton = () => {
+//     const { loginWithRedirect, isAuthenticated } = useAuth0();
+//     if (!isAuthenticated) {
+//         return <button className="btn btn-primary
+//             mx-5 my-5 px-4"
+//                        onClick={() => loginWithRedirect()}>
+//             Log In</button>;
+//     }
+// };
+//
+// export default LoginButton;
 
-const LoginButton = () => {
-    const { loginWithRedirect, isAuthenticated } = useAuth0();
-    if (!isAuthenticated) {
-        return <button className="btn btn-primary
-            mx-5 my-5 px-4"
-                       onClick={() => loginWithRedirect()}>
-            Log In</button>;
+class Login extends Component{
+
+    // initiate state
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: 'username',
+            password: 'password',
+        }
     }
-};
 
-export default LoginButton;
+
+    /* the rest of the code ... */
+
+handleInputChange = (event) => {
+    const target = event.target;
+    let value = event.target.value;
+    const name = target.name;
+
+
+    if (target.name === "password") {
+        document.getElementById(name).type = "password";
+        value = md5(event.target.value);
+    }
+
+
+    this.setState({
+        [name]: value
+    });
+}
+
+    setEmptyValue = (event) => {
+        const name = event.target.name
+        document.getElementById(name).value = "";
+    }
+    render(){
+    return (<div></div>)   }
+}
+export default Login;
