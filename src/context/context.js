9da5d3077d7d6da22d7779285558dbf2bd5317a9
jh8839 <jh8839@nyu.edu>
2023-12-15 08:04:14 -0500

    frontend logn post used fetch user_id

--- src/context/context.js
+++ src/context/context.js
@@ -1,13 +1,18 @@
 // Contexts.js
 import React, {createContext, useState} from 'react';
 
+// global state todos:
+const TodosContext = createContext({
+    todos: [], fetchTodos: () => {}
+})
+
 //authentication
 export const AuthContext = createContext(null);
 
 //user to display for the database query
 export const CurrentUserContext = createContext(null);
 
-//function to manage logged-in user
+//function to manage logged-in user // -- should i export to log in?
 export default function MyApp() {
     const [currentUser, setCurrentUser] = useState(null);
     return (
@@ -20,7 +25,6 @@ export default function MyApp() {
         </CurrentUserContext.Provider>
     );
 }
-
 
 function GetName() {
     const [name, setName] = useState('');
@@ -39,3 +43,26 @@ function GetName() {
     )
 }
 export default GetName;
+
+//component todos
+export default function Todos() {
+    const [todos, setTodos] = useState([])
+    const fetchTodos = async () => {
+        const response = await fetch("http://localhost:8000/todo")
+        const todos = await response.json()
+        setTodos(todos.data)
+    }
+}
+
+// get address, fetch address, get all addresses associated with a user, you have to fetch the user id from the flask backend
+
+useEffect(() => {
+    fetchAddresses()
+}, [])
+
+return (
+    <TodosContext.Provider value={{todos, fetchTodos}}>
+            {Addresses.map((Address) => (
+                {} ))}
+    </TodosContext.Provider>
+)
---
+++ src/types/requests.jsx
@@ -0,0 +1,17 @@
+// import {formatDate} from "@storybook/blocks";
+//
+// export type Put = {
+//     id: number;
+//     title: string;
+//     imageUrl: string;
+//     content: string;
+// };
+// export type Get = {
+//     id: number;
+//     day: formatDate();
+// };
+//
+// export type Get = {
+//     id: number;
+// };
+
